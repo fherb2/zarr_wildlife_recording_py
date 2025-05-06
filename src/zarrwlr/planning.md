bc2d5ea4205b474d6eab83a064cb27dd041928dc
fherb2 <herbrand@gmx.de>
2025-05-06 18:29:54 +0200

    Transfer projekt to other PC

--- src/zarrwlr/planning.md
+++
@@ -1,7 +0,0 @@
-# Common planning of the software parts
-
-## Including data structure into Zarr
-
-* Start point is a group somewhere in the data base: representing variable: *wlrec_grp*, type: Zarr-group-object, default value: under root of Zarr: "/wlrecsrc" (wildlife-recording-source)
-* Under this start point: we implement the designed structure from hdf5. Datasets are named "array" in Zarr.
-* First point of this next level: representing as variable: original_sound_file_grp, type: Zarr-group-object, default value: '/original_sound_files'
---
+++ src/zarrwlr/zarrwav.py
@@ -0,0 +1,38 @@
+
+
+import logging
+from pathlib import Path
+from io import BytesIO, BufferedReader
+import wave
+
+# get the module logger   
+logger = logging.getLogger(__name__)
+
+def is_wav_file(file: str | Path | BufferedReader) -> bool:
+    
+    if isinstance(file, str):
+        file = Path(file)
+    
+    is_wav = False
+    if isinstance(file, BufferedReader):
+        # read from start position of opened file and reset the
+        # pointer to the value before
+        current_position = file.tell()
+        with wave.open(file, 'rb') as wf:
+            try:
+                wf.getparams()
+                is_wav = True
+            except (wave.Error, EOFError):
+                pass
+        file.seek(current_position)
+    elif isinstance(file, Path):
+        with wave.open(file, 'rb') as wf:
+            try:
+                wf.getparams()
+                is_wav = True
+            except (wave.Error, EOFError):
+                pass
+    else:
+        raise ValueError("file must be of type: str, Path (both as a valid file path) or BufferedReader (as already opened file).")
+    
+    return is_wav
